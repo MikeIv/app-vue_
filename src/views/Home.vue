b27569a314cabe72899fb7ea3a976a026051fa55
MikeIv <210415@dantser.ru>
2021-06-24 15:24:28 +0300

    add block

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,12 @@
 
       <h1 class="app__h1">{{ title }}</h1>
 
+      <section class="app__block">
+        <h2 class="v-hidden">Простой блок</h2>
+        <input type="text" class="app__input">
+
+      </section>
+
     </div>
   </div>
 </template>

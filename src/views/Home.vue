9fd5874e4353668af4f738c2abffaf8cc89fbccf
MikeIv <210415@dantser.ru>
2021-06-25 16:05:40 +0300

    add block

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="app">
     <div class="app__wrapper">
-      <img alt="Vue logo" src="../assets/img/logo.png" class="app__logo">
-
-      <h1 class="app__h1">{{ title }}</h1>
+      <div class="app__header-top">
+        <img alt="Vue logo" src="../assets/img/logo.png" class="app__logo">
+        <h1 class="app__h1">{{ title }}</h1>
+      </div>
 
       <section class="app__block">
         <h2 class="v-hidden">Простой блок</h2>
+        <div class="app__row">
+          <p class="app__txt">{{ text }}</p>
+        </div>
+        <div class="app__row">
+          <p class="app__txt">{{ textLower }}</p>
+        </div>
+        <div class="app__row">
+          <p class="app__txt">{{ toLower }}</p>
+        </div>
         <input v-model="text" type="text" class="app__input">
-        <p class="app__txt">{{ text }}</p>
-        <button class="app__btn">Получить</button>
+
+        <button class="app__btn" @click="getText">Получить</button>
+        <button class="app__btn" @click="getLowerText">Нижний регистр</button>
+      </section>
+
+      <section class="app__block">
+        <h2 class="v-hidden">Простой блок</h2>
+        <div class="app__part">
+          <button class="app__btn" @click="showContent = !showContent">Показать блок</button>
+          <div v-if="showContent" class="app__row">
+            <p class="app__txt">{{ content }}</p>
+          </div>
+        </div>
 
       </section>
 
@@ -30,12 +51,27 @@ export default {
   },
   data: () => ({
     title: 'Проект на Vue',
-    text: ''
+    text: '',
+    textLower: '',
+
+  //  block 2
+    title2: 'v-if',
+    content: 'Содержание',
+    showContent: false
 
   }),
   methods: {
     getText () {
-
+      console.log('проверка' + this.text)
+      console.log(`значение текста ${this.text}`)
+    },
+    getLowerText () {
+      this.textLower = this.text.toLowerCase()
+    }
+  },
+  computed: {
+    toLower () {
+      return this.text.toLowerCase()
     }
   }
 

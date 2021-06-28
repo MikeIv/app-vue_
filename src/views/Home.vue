ce7dd0708a947023c45444786664d386aa91bf8d
Сотрудник <sotrudnik@Mac47.local>
2021-06-28 17:56:52 +0300

    add methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,6 +40,18 @@
       <section class="app__block app__block--width">
         <h2 class="v-hidden">Простой блок</h2>
         <h2 class="app__h1">{{ titleNotes }}</h2>
+
+        <!--    add note    -->
+        <div class="app__item-add">
+          <div class="app__txt-row">
+            <h3 class="app__h3">{{ message }}</h3>
+          </div>
+          <input v-model="note.title" type="text" class="app__item-input">
+          <textarea v-model="note.descr" class="app__item-textarea"></textarea>
+          <button @click="addNote" class="app__add-item-btn">Добавить заметку</button>
+        </div>
+
+        <!--    list of notes    -->
         <ul class="app__list">
           <li class="app__item-note" v-for="(note, index) in notes" :key="index">
             <h3 class="app__item-title">{{ note.title }}</h3>
@@ -55,6 +67,8 @@
     </div>
   </div>
 </template>
+
+
 
 
 
@@ -79,19 +93,24 @@ export default {
 
     // block 3 Notes
     titleNotes: 'Заметки',
+    message: null,
+    note: {
+      title: '',
+      descr: ''
+    },
     notes: [
       {
-        title: 'Загаловок №1',
+        title: 'Заголовок №1',
         descr: 'Содержание заметки',
         date: new Date(Date.now()).toLocaleString()
       },
       {
-        title: 'Загаловок №2',
+        title: 'Заголовок №2',
         descr: 'Содержание заметки',
         date: new Date(Date.now()).toLocaleString()
       },
       {
-        title: 'Загаловок №3',
+        title: 'Заголовок №3',
         descr: 'Содержание заметки',
         date: new Date(Date.now()).toLocaleString()
       }
@@ -107,9 +126,30 @@ export default {
       this.textLower = this.text.toLowerCase()
     },
 
-  //  block2
+    //  block2
     getContent () {
       this.showContent = !this.showContent
+    },
+
+    //  App
+    addNote () {
+      console.log(this.note)
+
+      let {title, descr} = this.note
+
+      if(title === '') {
+        this.message = 'Напишите название заголовка'
+        return false
+      }
+
+      this.notes.push ({
+        title,
+        descr,
+        date: new Date(Date.now()).toLocaleString(),
+      })
+      this.note.title = ''
+      this.note.descr = ''
+      this.message = null
     }
   },
   computed: {

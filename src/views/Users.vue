e6283c27285747adce8e3070be520e47ad73a003
Mike <mikeuivanov@gmail.com>
2021-09-19 16:49:23 +0300

    add axios

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,47 +1,59 @@
 <template>
-<div class="app">
-  <section class="app__wrapper">
-    <div class="users">
-      <h1 class="users__h1">Пользователи</h1>
-      <table class="users__table">
-        <thead>
-        <tr>
-          <th class="users__table-th">Имя</th>
-          <th class="users__table-th">Возраст</th>
-          <th class="users__table-th">Пол</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td class="users__table-td">Анна</td>
-          <td class="users__table-td">33</td>
-          <td class="users__table-td">женский</td>
-        </tr>
-        <tr>
-          <td class="users__table-td">Оля</td>
-          <td class="users__table-td">43</td>
-          <td class="users__table-td">женский</td>
-        </tr>
+  <div class="app">
+    <section class="app__wrapper">
+      <div class="users">
+        <h1 class="users__h1">Пользователи</h1>
+        <table class="users__table">
+          <thead>
+          <tr class="users__table-row">
+            <th class="users__table-th">Имя</th>
+            <th class="users__table-th">Счет </th>
+            <th class="users__table-th">Пол</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="user in users" :key="user.id" class="users__table-row">
+            <td class="users__table-td">{{ user.name }}</td>
+            <td class="users__table-td">{{ user.count }}</td>
+            <td class="users__table-td">{{ user.gender }}</td>
+          </tr>
+          </tbody>
+        </table>
 
-        </tbody>
-      </table>
-
-    </div>
-  </section>
-</div>
+        <div class="">{{ users }}</div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "Users",
 
   data: () => ({
-    users: [
-      { id: 1, name: 'Anna', age: 33, gender : 'female' },
-      { id: 1, name: 'Olya', age: 43, gender : 'female' }
-    ]
+    users: []
 
   }),
+  mounted() {
+    axios
+        .get('https://api.genderize.io/?name[]=peter&name[]=lois&name[]=stevie&name[]=anna')
+        .then(response => (this.users = response.data));
+
+    console.log(this.users)
+  }
+  // created() {
+  //   axios
+  //       .post('https://api.myjson.com/bins/rzgya')
+  //       .then(response => {
+  //         console.log(response)
+  //       })
+  //   // this.users =[
+  //   //   { id: 1, name: 'Anna', age: 33, gender : 'female' },
+  //   //   { id: 2, name: 'Olya', age: 48, gender : 'female' }
+  //   // ]
+  // }
 }
 </script>
 
